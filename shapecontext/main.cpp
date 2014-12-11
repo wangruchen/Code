eb37b8122f2c999381df56661d2a6a751f3213f2
wangruchen <lanyingfenfei@126.com>
2014-12-11 22:29:11 +0800

    about shape context

--- shapecontext/main.cpp
+++ shapecontext/main.cpp
@@ -52,13 +52,12 @@ int main(int argc, char* argv[])
     Mat firstIm=imread(firstname.str(),0);
     //imshow("firstpicture",firstIm);
     vector<Point> firstsimpcont=simplecontour(firstIm);
-    for(int l=1;l<=8;l++)
-    {
+
         for(int k=1;k<=5;k++)
         {
             stringstream kname;
 
-            kname<<"0"<<l<<"0"<<k<<"_m.bmp";
+            kname<<path<<k<<"_m.bmp";
             Mat kIm=imread(kname.str(),0);
             vector<Point> ksimpcont=simplecontour(kIm);
             dis[k-1] = mysc -> computeDistance(firstsimpcont,ksimpcont);
@@ -70,31 +69,8 @@ int main(int argc, char* argv[])
                 num=k;
             }
             cout<<"distance between "<<firstname.str()<<" and "<<kname.str()<<" is "<<dis[k-1]<<endl;
-    //        stringstream kname1;
-    //        kname1<<path<<1<<"_m.bmp";
-    //        Mat kIm1=imread(kname1.str(),0);
-    //        vector<Point> ksimpcont1=simplecontour(kIm1);
-    //        dis[1] = mysc -> computeDistance(firstsimpcont,ksimpcont1);
-    //        //imshow("picture",kIm);
-    //        cout<<"distance between "<<firstname.str()<<" and "<<kname1.str()<<" is "<<dis[1]<<endl;
-    //        stringstream kname2;
-    //        kname2<<path<<2<<"_m.bmp";
-    //        Mat kIm2=imread(kname2.str(),0);
-    //        vector<Point> ksimpcont2=simplecontour(kIm2);
-    //        dis[2] = mysc -> computeDistance(firstsimpcont,ksimpcont2);
-    //        //imshow("picture",kIm);
-    //        cout<<"distance between "<<firstname.str()<<" and "<<kname2.str()<<" is "<<dis[2]<<endl;
-    //        //waitKey();
-    //        stringstream kname3;
-    //        kname3<<path<<3<<"_m.bmp";
-    //        Mat kIm3=imread(kname3.str(),0);
-    //        vector<Point> ksimpcont3=simplecontour(kIm3);
-    //        dis[3] = mysc -> computeDistance(firstsimpcont,ksimpcont3);
-    //        //imshow("picture",kIm);
-    //        cout<<"distance between "<<firstname.str()<<" and "<<kname3.str()<<" is "<<dis[3]<<endl;
-
         }
-    }
+
     cout<<"best distance "<<" is "<<mindis<<" between "<<firstname.str()<<"and"<<path<<num<<endl;
 
 }
